6cac08118f4acb6e905ed3dda6f9432e7a077788
marey27 <abdallahgamal027@gmail.com>
2025-05-04 12:45:24 +0000

    yarb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   "env": {
-    "browser": true,   // if you're targeting the browser
-    "es2021": true,
-    "node": true        // Add Node.js environment
+    "node": true,
+    "es2021": true
   },
   "extends": ["eslint:recommended"],
   "parserOptions": {
@@ -10,9 +9,8 @@ module.exports = {
     "sourceType": "module"
   },
   "rules": {
-    "no-console": "warn",  // Downgrade to warning for console statements
-    "semi": ["error", "always"],  // Enforce semicolons
-    "no-undef": "off"  // Optionally, turn off no-undef if you'd rather disable this rule
+    "no-console": "off", // Disable the warning for console statements
+    "semi": ["error", "always"]
   }
 };
 
