b1573036620fdd1c7b025351c1f5539976bf45ad
marey27 <abdallahgamal027@gmail.com>
2025-05-04 12:40:45 +0000

    eeeeeeeeeeeeeeee

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
+  "env": {
+    "browser": true,
+    "es2021": true
   },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 'latest'
+  "extends": ["eslint:recommended"],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
   },
-  rules: {
-    "indent": ["error", 2] // Force 2-space indentation
-    
-    semi: ['error', 'always']  // Enforce semicolons
+  "rules": {
+    "no-console": "warn",  // Downgrade to warning
+    "semi": ["error", "always"]  // Enforce semicolons
   }
 };
+
